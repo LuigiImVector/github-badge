9e1d4cde73251aa2937e0c0a1ac8fe865d9a7efc
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-15 10:39:26 +0200

    Testing stackoverflow's answer

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function changeNumber() {
-	document.getElementById("years-number").textContent = getValue;
+import pupa from 'pupa'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { dirname } from 'dirname-filename-esm'
+
+const __dirname = dirname(import.meta)
+const template = readFileSync(join(__dirname, '../badge.svg'), 'utf8')
+
+export default (req, res) => {
+  let { years } = req.query
+  if (typeof years !== 'string') return res.status(400).end()
+  res.setHeader('Content-Type', 'text/xml')
+  res.end(pupa(template, { years }))
 }
-
-let getValue;
-
-window.addEventListener("load", function () {
-	let name="years";
-	if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-      getValue = decodeURIComponent(name[1]);
-  
-	changeNumber();
-});
