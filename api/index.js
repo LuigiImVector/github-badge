232e67129a782591f73e4730338a8ffab9c58355
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-17 11:28:55 +0200

    Test multiparameters

--- api/index.js
+++ api/index.js
@@ -1,13 +1,28 @@
+/*function colorBadge(color) {
+  switch(color) {
+    case "red":
+      return readFileSync(join(__dirname, '../red.svg'), 'utf8');
+    case "blue":
+      return readFileSync(join(__dirname, '../blue.svg'), 'utf8');
+    default:
+      return 0;
+  }
+}*/
+
+
+// Thanks to https://stackoverflow.com/questions/69164294/create-a-github-badge
 import pupa from 'pupa'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import { dirname } from 'dirname-filename-esm'
 
-const __dirname = dirname(import.meta)
-const template = readFileSync(join(__dirname, '../red.svg'), 'utf8')
+const __dirname = dirname(import.meta);
+const template = readFileSync(join(__dirname, '../blue.svg'), 'utf8');
 
 export default (req, res) => {
-  let { years } = req.query
+  let { years } = req.query.color
+  //let { color } = req.query.color
+  //const template = colorBadge({color});
   if (typeof years !== 'string') return res.status(400).end()
   res.setHeader('Content-Type', 'image/svg+xml')
   res.end(pupa(template, { years }))
