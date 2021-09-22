e470703d6259d6d4d3c020e6692adafcf8ab83fc
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-22 19:14:28 +0200

    Fixed 500: INTERNAL_SERVER_ERROR

--- api/blue/index.js
+++ api/blue/index.js
@@ -2,6 +2,15 @@ import pupa from 'pupa'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import { dirname } from 'dirname-filename-esm'
-import  { badgeYears } from '../index.js'
 
-badgeYears();
+const __dirname = dirname(import.meta);
+const template = readFileSync(join(__dirname, '*.svg'), 'utf8');
+
+export default (req, res) => {
+  let { years } = req.query
+  //let { color } = req.query.color
+  //const template = colorBadge({color});
+  if (typeof years !== 'string') return res.status(400).end()
+  res.setHeader('Content-Type', 'image/svg+xml')
+  res.end(pupa(template, { years }))
+}
