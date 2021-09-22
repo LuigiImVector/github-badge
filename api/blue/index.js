344298ddfceac30ac2ae7c25726ac3ac0408bd03
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-22 19:12:12 +0200

    Two badge added (red & blue)

---
+++ api/blue/index.js
@@ -0,0 +1,7 @@
+import pupa from 'pupa'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { dirname } from 'dirname-filename-esm'
+import  { badgeYears } from '../index.js'
+
+badgeYears();
--- api/index.js
+++ api/index.js
@@ -4,8 +4,10 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 import { dirname } from 'dirname-filename-esm'
 
+
+export function badgeYears () {
 const __dirname = dirname(import.meta);
-const template = readFileSync(join(__dirname, '../blue.svg'), 'utf8');
+const template = readFileSync(join(__dirname, 'image.svg'), 'utf8');
 
 export default (req, res) => {
   let { years } = req.query
@@ -14,4 +16,5 @@ export default (req, res) => {
   if (typeof years !== 'string') return res.status(400).end()
   res.setHeader('Content-Type', 'image/svg+xml')
   res.end(pupa(template, { years }))
+}
 }
