4c8a2dacaf44c7e8122267ba158c0dc8e7cc05bc
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-22 20:33:46 +0200

    Test

--- api/index.js
+++ api/index.js
@@ -5,15 +5,28 @@ import { join } from 'path'
 import { dirname } from 'dirname-filename-esm'
 
 const __dirname = dirname(import.meta);
+const template = readFileSync(join(__dirname, '../red.svg'), 'utf8');
+
 
 
 export default (req, res) => {
-  let { years } = req.query
-  //let color = req.query.color
-  const template = readFileSync(join(__dirname, '../blue.svg'), 'utf8');
+  let { years } = req.query.years
+  let color = req.query.color
 
+  if(color == "red") {
+    let { first } = "49B2F8F5"
+    let { second } = "49B2F8"
+  } else {
+    let { first } = "49B2F8F5"
+    let { second } = "49B2F8"
+  }
+
+
+  
+  //let color = req.query.color
+  
   //const template = colorBadge({color});
   if (typeof years !== 'string') return res.status(400).end()
   res.setHeader('Content-Type', 'image/svg+xml')
-  res.end(pupa(template, { years }))
+  res.end(pupa(template, [{ first }, { second }, { years }]))
 }
