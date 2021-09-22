4c82b5f52b28f50ce51d5ac9f4af1e3d87226d2b
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-22 19:22:47 +0200

    Fixed 500: INTERNAL_SERVER_ERROR from 'blue' folder

--- api/blue/index.js
+++ api/blue/index.js
@@ -4,12 +4,10 @@ import { join } from 'path'
 import { dirname } from 'dirname-filename-esm'
 
 const __dirname = dirname(import.meta);
-const template = readFileSync(join(__dirname, '*.svg'), 'utf8');
+const template = readFileSync(join(__dirname, 'badge.svg'), 'utf8');
 
 export default (req, res) => {
   let { years } = req.query
-  //let { color } = req.query.color
-  //const template = colorBadge({color});
   if (typeof years !== 'string') return res.status(400).end()
   res.setHeader('Content-Type', 'image/svg+xml')
   res.end(pupa(template, { years }))
--- api/index.js
+++ api/index.js
@@ -4,17 +4,30 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 import { dirname } from 'dirname-filename-esm'
 
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
-export function badgeYears () {
 const __dirname = dirname(import.meta);
-const template = readFileSync(join(__dirname, 'image.svg'), 'utf8');
 
 export default (req, res) => {
-  let { years } = req.query
+
+  if(getParameterByName("color")="red") {
+    const template = readFileSync(join(__dirname, 'red.svg'), 'utf8');
+  } else if (getParameterByName("color")="blue") {
+    const template = readFileSync(join(__dirname, 'blue.svg'), 'utf8');
+  }
+
+
+  let { years } = getParameterByName("years");
   //let { color } = req.query.color
   //const template = colorBadge({color});
   if (typeof years !== 'string') return res.status(400).end()
   res.setHeader('Content-Type', 'image/svg+xml')
   res.end(pupa(template, { years }))
-}
 }
