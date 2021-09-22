5d9450a468779ba8580df57372d0616532959103
LuigiImVector <luigi.vector15@virgilio.it>
2021-09-22 20:43:22 +0200

    Test

--- api/index.js
+++ api/index.js
@@ -10,15 +10,15 @@ const template = readFileSync(join(__dirname, '../red.svg'), 'utf8');
 
 
 export default (req, res) => {
-  let { years } = req.query.years
+  let years = req.query.years
   let color = req.query.color
 
   if(color == "red") {
-    let { first } = "49B2F8F5"
-    let { second } = "49B2F8"
+    let first = "49B2F8F5"
+    let second = "49B2F8"
   } else {
-    let { first } = "49B2F8F5"
-    let { second } = "49B2F8"
+    let first = "49B2F8F5"
+    let second = "49B2F8"
   }
 
 
@@ -28,5 +28,5 @@ export default (req, res) => {
   //const template = colorBadge({color});
   if (typeof years !== 'string') return res.status(400).end()
   res.setHeader('Content-Type', 'image/svg+xml')
-  res.end(pupa(template, [{ first }, { second }, { years }]))
+  res.end(pupa(template, [first, second, years]))
 }
